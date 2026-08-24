93ed7d52ab5c95fb2169146db118c52a5bc9fb32
Hung127 <hunghungtt05@gmail.com>
2026-08-24 16:38:16 +0700

    feat: added basic listening loop mechanism

--- src/com/example/web/HTTPServer.java
+++ src/com/example/web/HTTPServer.java
@@ -2,12 +2,14 @@ package com.example.web;
 
 import com.example.web.configuration.ConfigurationManager;
 import java.nio.charset.StandardCharsets;
-
+import java.util.Scanner;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 
 import com.example.web.configuration.Configuration;
@@ -35,6 +37,30 @@ public class HTTPServer {
         return html;
     }
 
+    private void readRequest(InputStream inputStream) throws IOException {
+        // make a inputstreamreader to make inputstream bytes into utf8 characters
+        InputStreamReader inputStreamReader = new InputStreamReader(inputStream,
+                StandardCharsets.UTF_8);
+        // we cannot use inputStream to read all bytes, because the readAllBytes stops
+        // iff the connection is close, maybe the browser wants to keep the connection
+        // -> cant read all bytes at once
+
+        // make buffer reader to read inputreader line by line
+        BufferedReader bufferReader = new BufferedReader(inputStreamReader);
+
+        String line = null;
+        // line = null means there is no more data (closed connection)
+        while ((line = bufferReader.readLine()) != null) {
+            // problem: maybe the browser just keep the connection
+            // so we want a way to stop when a request header is sent -> the end of a
+            // request header is a blank line -> isEmpty() ("")
+            if (line.isEmpty()) {
+                break;
+            }
+            System.out.println("Received: " + line);
+        }
+    }
+
     public String makeHTTPResponse(String body) {
         byte[] bodyBytes = body.getBytes(StandardCharsets.UTF_8);
 
@@ -58,38 +84,48 @@ public class HTTPServer {
         }
         Configuration config = configManager.getCurrentConfiguration();
 
-        // try {
-        // // System.out.println(Json.stringify(config, true));
-        // } catch (Exception e) {
-        // System.out.println(e.toString());
-        // }
-
         int port = config.getPort();
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Creating socket with port " + port + "...");
         try {
             ServerSocket server = new ServerSocket(port);
             System.out.println("Server is listening on port " + port);
-            Socket socket = server.accept();
-            InputStream inputStream = socket.getInputStream();
-            OutputStream outputStream = socket.getOutputStream();
+            while (true) {
+                Socket socket = server.accept();
+                InputStream inputStream = socket.getInputStream();
+                OutputStream outputStream = socket.getOutputStream();
+                HTTPServer httpServer = new HTTPServer();
 
-            // TODO: Reading
-            // DO IT
+                // TODO: Reading
+                try {
+                    httpServer.readRequest(inputStream);
+                } catch (IOException e) {
+                    System.out.println("Failed to read request header from client");
+                    socket.close();
+                    server.close();
+                    scanner.close();
+                    return;
+                }
 
-            // TODO: Writing
-            HTTPServer httpServer = new HTTPServer();
-            String response = httpServer.makeHTTPResponse(httpServer.getHTML());
-            outputStream.write(response.getBytes());
+                // TODO: Writing
+                String response = httpServer.makeHTTPResponse(httpServer.getHTML());
+                outputStream.write(response.getBytes());
 
-            System.out.println("Sent: " + response);
+                System.out.println("Sent: " + response);
 
-            System.out.println("Server is closing...");
-            socket.close();
-            server.close();
-            System.out.println("Server closed");
-
+                System.out.print("Do you want to stop: ");
+                String closed = scanner.nextLine();
+                if (closed.compareToIgnoreCase("YES") == 0) {
+                    System.out.println("Server is closing...");
+                    socket.close();
+                    server.close();
+                    System.out.println("Server closed");
+                    break;
+                }
+            }
         } catch (IOException e) {
             System.out.println("Exception: " + e.toString());
         }
+        scanner.close();
     }
 }
